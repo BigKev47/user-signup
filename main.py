1b9c72ff57e2cbeaa40a4d089da06dfdd689ed8f
Kevin Corlett <mediumkev@gmail.com>
2017-10-05 15:26:03 -0500

    Seems to be up and rocking.

--- main.py
+++ main.py
@@ -3,48 +3,50 @@ import cgi
 #~
 app = Flask(__name__)
 #~
-app.config['DEBUG'] = True      # displays runtime errors in the browser, too
+app.config['DEBUG'] = True      # displays runtime errors in the browser..
 
+@app.route("/submit", methods=["POST"])
 def process_form():
     user = request.form['username']
     password = request.form['password']
     verify = request.form['verify']
     email = request.form['email']
-    results = [is_valid(user),isvalid(password),password_match(verify),valid_email(email)]
+    results = [is_valid(user),is_valid(password),password_match(password, verify),valid_email(email)]
     if results == ["","","",""]:
-        return True
+        return render_template('welcome.html', username = user)
     else:
-        return results
+        return render_template('signup.html', results = results, username = user, email = email)
     
 
 def is_valid(response):
     if response != "":
         if " " not in response:
             if 2<len(response)<21:
-                return True
+                return ""
             return "Field must be between 3-20 characters."
         return "Field cannot contain spaces."
-    return "No input"
+    else:
+        return "No input"
 
 def valid_email(email):
+    if is_valid(email) == "No input":
+                return ""
     if email.count("@") == 1:
         if email.count(".") == 1:
-            if is_valid(email) == "No input":
-                return True
             return is_valid(email)
         return "email must contain on '.', one '@', and be between 3-20 characters long."
         
 def password_match(first_PW,conf):
-    if first_pw == conf:
+    if first_PW == conf:
         return ""
     else:
         return "Passwords do not match."
     
-@app.route("/")
-def main():
-    #encoded_error = request.args.get("error")
-    return render_template('signup.html', results = process_form(), usernamep=request.form['username'], error=encoded_error and cgi.escape(encoded_error, quote=True))
+@app.route("/", methods=["GET","POST"])
+def index():
+    encoded_error = request.args.get("error")
+    return render_template('signup.html', results = [], error=encoded_error and cgi.escape(encoded_error, quote=True))
     #~
-    print(valid_email("kevin@unionmaccom"))
+    
 
-main()
+app.run()
